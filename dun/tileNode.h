a5d708afa561ba9f031dff638dc0d349db6de44d
문희진 <40350423+orange492@users.noreply.github.com>
2018-07-17 12:50:26 +0900

    맵이미지

--- dun/tileNode.h
+++ dun/tileNode.h
@@ -4,38 +4,34 @@
 #define _TILE_H_
 
 // 타일 갯수(50,50)은 맵의 기본입니당~~, 수정하면 커밋은 바꿔서 올려주세용~~
-#define TILEX 80
-#define TILEY 50
+#define TILEX 100
+#define TILEY 100
 //(80,25)는 타운맵입니당
 //#define TILEX 80
 //#define TILEY 25
 
 // 전체 맵 사이즈 입니다   수정 부우탁 드립니다
-#define BACKGROUNDSIZEX TILEX*96
-#define BACKGROUNDSIZEY TILEY*96
+//#define BACKGROUNDSIZEX TILEX*TILESIZE
+//#define BACKGROUNDSIZEY TILEY*TILESIZE
 
 //작은 카메라 사이즈
 #define CAMERAX 1100
 #define CAMERAY 1080
 
 // 타일 규격
-#define TILESIZE 96
+#define TILESIZE 32
 #define TILESIZE2 32
-
-
-//#define TILEX 28
-//#define TILEY 12
 
 // 타일 총 사이즈
 #define TILESIZEX TILESIZE * TILEX
 #define TILESIZEY TILESIZE * TILEY
 
 // 타일셋 갯수
-#define SAMPLETILEX 23
-#define SAMPLETILEY 26
+#define SAMPLETILEX 41
+#define SAMPLETILEY 51
 
 // 맵이름
-#define MAPNAME "map/Dungeon1(28x11).map"
+#define MAPNAME "map/myDungen.map"
 
 // 지형	
 enum TERRAIN
